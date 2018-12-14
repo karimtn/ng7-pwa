220f4dc0fa2d50ea2af33490cd7e1ffe743c5a04
Med Karim Bchini <mohkarb@gmail.com>
2018-12-14 19:41:14 +0100

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) {}
+
+  getQuotes() {
+    return this.http.get("https://talaikis.com/api/quotes/random/");
+  }
+
+}
